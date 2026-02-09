9327af9cf8bd482f5047b1a7c5f518f6a19386f1
Jin <jj1430413@gmail.com>
2026-02-09 16:41:14 +0900

    로컬에서 업데이트 수정

--- src/kosta/service/GoodsService.java
+++ src/kosta/service/GoodsService.java
@@ -7,5 +7,6 @@ public static void main(String[] args) {
 	public void select() {
 		System.out.println("select 추가 ...");
 		System.out.println("시나리오 3번 test");
+		System.out.println("로컬에서 수정");
 	}
 }
