2af2b16c8a7c3d49cb120398edb6d303e5b6e30b
Jin <jj1430413@gmail.com>
2026-02-09 14:43:00 +0900

    파일 수정1

---
+++ src/kosta/controller/GoodsController.java
@@ -0,0 +1,8 @@
+package kosta.controller;
+
+public class GoodsController {
+	public static void main(String[] args) {
+		System.out.println("test...");
+		System.out.println("수정..");
+	}
+}
---
+++ src/kosta/view/MainView.java
@@ -0,0 +1,11 @@
+package kosta.view;
+
+public class MainView {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+		
+	}
+
+}
