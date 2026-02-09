d9d55bc19041ba32fbbe059c7dd83c321aa2a933
jin1430 <jj1430413@gmail.com>
2026-02-09 15:52:26 +0900

    시나리오02 remote에만 변경사항 있을때

--- src/kosta/controller/GoodsController.java
+++ src/kosta/controller/GoodsController.java
@@ -1,8 +1,5 @@
 package kosta.controller;
 
 public class GoodsController {
-	public static void main(String[] args) {
-		System.out.println("test....");
-		System.out.println("수정..");
-	}
+	int i = 10;
 }
