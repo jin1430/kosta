74946da3c246eeb0e484cc1274e784667bdf5906
jin1430 <jj1430413@gmail.com>
2026-02-09 16:57:26 +0900

    Add test log message in GoodsController

--- src/kosta/controller/GoodsController.java
+++ src/kosta/controller/GoodsController.java
@@ -3,7 +3,7 @@ package kosta.controller;
 public class GoodsController {
 	int i = 10;
 	public void test() {
-	
+		System.out.println("원격에서 test 추가함 - 시나리오 5번 테스트");
 	}
 	public void selectAll() {
 
