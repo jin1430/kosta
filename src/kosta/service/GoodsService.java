8bea1f875e35e9f5eda6b6cc3a3579910d0e0a81
Jin <jj1430413@gmail.com>
2026-02-09 16:44:05 +0900

    로컬에서 메인  수정

--- src/kosta/service/GoodsService.java
+++ src/kosta/service/GoodsService.java
@@ -1,12 +1,15 @@
 package kosta.service;
 
 public class GoodsService {
-public static void main(String[] args) {
-	System.out.println("insert call...");
+	public void insert() {
+		System.out.println("insert call.. ");
 	}
 	public void select() {
 		System.out.println("select 추가 ...");
 		System.out.println("시나리오 3번 test");
+		
+	}
+	public void update() {
 		System.out.println("로컬에서 업데이트 수정");
 	}
 }
