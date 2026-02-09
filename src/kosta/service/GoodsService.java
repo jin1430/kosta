96e231e06be9423271ea5c17214541427af75071
jin1430 <jj1430413@gmail.com>
2026-02-09 16:45:37 +0900

    delete 추가

--- src/kosta/service/GoodsService.java
+++ src/kosta/service/GoodsService.java
@@ -8,4 +8,7 @@ public static void main(String[] args) {
 		System.out.println("select 추가 ...");
 		System.out.println("시나리오 3번 test");
 	}
+	public void delete() {
+		System.out.println("delete 추가 ...");
+	}
 }
