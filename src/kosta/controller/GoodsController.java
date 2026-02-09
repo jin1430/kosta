37cf842bbe04f08a17480548fdd279e7acaa8f39
Jin <jj1430413@gmail.com>
2026-02-09 14:47:47 +0900

    controller 추가, 메인 수정

--- src/kosta/controller/GoodsController.java
+++ src/kosta/controller/GoodsController.java
@@ -2,7 +2,7 @@ package kosta.controller;
 
 public class GoodsController {
 	public static void main(String[] args) {
-		System.out.println("test...");
+		System.out.println("test....");
 		System.out.println("수정..");
 	}
 }
