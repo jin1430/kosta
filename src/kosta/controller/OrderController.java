8b242754fd37df336ce6883d5c072e24f5024a61
Jin <jj1430413@gmail.com>
2026-02-10 12:28:49 +0900

    오타수정

--- src/kosta/controller/OrderController.java
+++ src/kosta/controller/OrderController.java
@@ -8,7 +8,7 @@ public class OrderController {
 	
 	public void orderCart() {
 		System.out.println("로컬 cart 브랜치에서 추가함.");
-
+	}
 
 	public void remoteCart() {
 		System.out.println("원격 main 브랜치에서 추가함.");
